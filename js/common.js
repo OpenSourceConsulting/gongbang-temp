47a4a3fcfdbfab73a5e9f4a2247fd4ea87c63152
gijeong <gjkim@osci.kr>
2019-12-10 15:48:24 +0900

    반응형 작업

--- js/common.js
+++ js/common.js
@@ -7,3 +7,45 @@ $(document).ready(function() {
 		$('#header').removeClass('active');
   });
 })
+
+$(document).ready(function(){
+  $("#devopsMsa #tab-01").click(function(){
+      $("#tab-02-contents").css("display", "none");
+      $("#tab-01-contents").css("display", "block");
+  });
+
+  $("#devopsMsa #tab-02").click(function(){
+    $("#tab-01-contents").css("display", "none");
+    $("#tab-02-contents").css("display", "block");
+  });
+
+  $("#migration .section-3 #tab-02").click(function(){
+    $("#tab-01-contents").css("display", "none");
+    $("#tab-02-contents").css("display", "block");
+  });
+
+  $("#migration .section-3 #tab-03").click(function(){
+    $("#tab-01-contents").css("display", "none");
+    $("#tab-03-contents").css("display", "block");
+  });
+
+  $("#migration .section-4 #tab-01").click(function(){
+    $("#tab-02-contents").css("display", "none");
+    $("#tab-01-contents").css("display", "block");
+  });
+
+  $("#migration .section-4 #tab-03").click(function(){
+    $("#tab-02-contents").css("display", "none");
+    $("#tab-03-contents").css("display", "block");
+  });
+
+  $("#migration .section-5 #tab-01").click(function(){
+    $("#tab-03-contents").css("display", "none");
+    $("#tab-01-contents").css("display", "block");
+  });
+
+  $("#migration .section-5 #tab-02").click(function(){
+    $("#tab-03-contents").css("display", "none");
+    $("#tab-02-contents").css("display", "block");
+  });
+});
